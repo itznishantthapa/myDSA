f929c8d8cf8c20444f9ea0f8bb79e411c4d9964e
Nishant Thapa <itsnishantu@gmail.com>
2024-08-03 23:39:09 +0545

    minor change

--- PracticeStack.java
+++ PracticeStack.java
@@ -54,8 +54,8 @@ public  class PracticeStack{
         s.push(10);
         s.push(20);
         s.push(30);
-        s.push(40);
-        s.push(40);
+        s.push(50);
+    
 
         s.display();
       System.out.println(s.isEmpty());
